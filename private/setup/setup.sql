8166b0770f1a8eb7ef98966203e3fd12d4cfb714
Marc Murphy <marc.murphy@kcl.ac.com>
2018-11-18 18:08:38 +0000

    Add SQL sintax to setup the database

---
+++ private/setup/setup.sql
@@ -0,0 +1,72 @@
+
+CREATE TABLE Member
+(
+  memberID    INT           NOT NULL,
+  title       VARCHAR(3)    NOT NULL,
+  firstname   VARCHAR(20)   NOT NULL,
+  surname     VARCHAR(30)   NOT NULL,
+  DoB         DATE,
+  phoneNo     CHAR(11),
+  violations  INT,
+  debt        INT,
+  email       VARCHAR(30),
+  homeAdress  VARCHAR(60),
+  PRIMARY KEY (memberID)
+);
+
+CREATE TABLE Rental
+(
+  rentalID    INT NOT NULL,
+  memberID    INT NOT NULL,
+  gameID      INT NOT NULL,
+  extension   INT,
+  period      INT NOT NULL,
+  startDate   DATE NOT NULL,
+  endDate     DATE NOT NULL,
+  PRIMARY KEY (rentalID),
+  FOREIGN KEY (memberID) REFERENCES Member(memberID),
+  FOREIGN KEY (gameID) REFERENCES Game(gameID)
+);
+
+CREATE TABLE Game
+(
+  gameID                INT           NOT NULL,
+  cost                  INT           NOT NULL,
+  type                  VARCHAR(20),
+  platform              VARCHAR(20)   NOT NULL,
+  ageLimit              INT           NOT NULL,
+  name                  VARCHAR(40)   NOT NULL,
+  isCurrentlyAvailable  BOOLEAN       NOT NULL,
+  releaseYear           INT,
+  PRIMARY KEY (gameID)
+);
+
+CREATE TABLE Ban
+(
+  memberID    INT   NOT NULL,
+  startDate   DATE  NOT NULL,
+  endDate     DATE  NOT NULL,
+  period      INT   NOT NULL,
+  FOREIGN KEY (memberID) REFERENCES Member(memberID),
+);
+
+CREATE TABLE Admin
+(
+  staffID INT NOT NULL,
+  isCurrent INT NOT NULL
+  FOREIGN KEY (staffID) REFERENCES Staff(staffID),
+);
+
+CREATE TABLE Staff
+(
+  staffID     INT           NOT NULL,
+  password    VARCHAR(20)   NOT NULL,
+  title       VARCHAR(3)    NOT NULL,
+  firstname   VARCHAR(20)   NOT NULL,
+  surname     VARCHAR(30)   NOT NULL,
+  DoB         DATE,
+  phoneNo     CHAR(11),
+  email       VARCHAR(30),
+  homeAdress  VARCHAR(60),
+  PRIMARY KEY (staffID)
+);
