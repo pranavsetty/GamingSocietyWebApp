48ea27f82494d1daea615e8133effbde4329b465
Karolina <karolina.szafranek@kcl.ac.uk>
2018-11-26 19:00:17 +0000

    Add dashboard tabs

---
+++ public/staff/adminTab.php
@@ -0,0 +1,126 @@
+<div class="justify-content-between flex-wrap flex-md-nowrap align-items-center text-center mb-5 mt-5">
+    <h1 class="align-left">Staff</h1>
+    <a class="btn btn-add align-right" href="#" title="Add member of staff"><i class="fas fa-plus"></i></a>
+    <div class="clear-float"></div>
+</div>
+<div class="row mt-3">
+    <div class="col">
+        <div class="card card-blue card-big">
+            <div class="card-title title-blue">
+                <div class="align-left label">Staff </div>
+                <div class="align-right">3</div>
+                <div class="clear-float"></div>
+            </div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th class="no-border" scope="col">Member Name</th>
+                        <th class="no-border" scope="col">Date of Birth</th>
+                        <th class="no-border" scope="col">Phone Number</th>
+                        <th class="no-border" scope="col">Email</th>
+                        <th class="no-border" scope="col">Home address</th>
+                        <th class="no-border" scope="col">Violations</th>
+                        <th class="no-border" scope="col"></th>
+                        <th class="no-border" scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Doug Judy</td>
+                        <td>02/07/1982</td>
+                        <td>07951938363</td>
+                        <td>pontiacb@gmail.com</td>
+                        <td>some address</td>
+                        <td>3</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    <tr>
+                        <td>Adrian Pimento</td>
+                        <td>22/02/1980</td>
+                        <td>07951938363</td>
+                        <td>pima@gmail.com</td>
+                        <td>some address</td>
+                        <td>10</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    <tr>
+                        <td>Keith Pembroke</td>
+                        <td>10/11/1979</td>
+                        <td>07951938363</td>
+                        <td>keith.pembroke@gmail.com</td>
+                        <td>some address</td>
+                        <td>2</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer"></div>
+        </div>
+    </div>
+</div>
+
+<div class="row mt-5">
+    <div class="col">
+        <div class="card card-purple card-big">
+            <div class="card-title title-purple">
+                <div class="align-left label">Rules </div>
+                <div class="align-right">3</div>
+                <div class="clear-float"></div>
+            </div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th class="no-border" scope="col">Member Name</th>
+                        <th class="no-border" scope="col">Date of Birth</th>
+                        <th class="no-border" scope="col">Phone Number</th>
+                        <th class="no-border" scope="col">Email</th>
+                        <th class="no-border" scope="col">Home address</th>
+                        <th class="no-border" scope="col">Violations</th>
+                        <th class="no-border" scope="col"></th>
+                        <th class="no-border" scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Doug Judy</td>
+                        <td>02/07/1982</td>
+                        <td>07951938363</td>
+                        <td>pontiacb@gmail.com</td>
+                        <td>some address</td>
+                        <td>3</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    <tr>
+                        <td>Adrian Pimento</td>
+                        <td>22/02/1980</td>
+                        <td>07951938363</td>
+                        <td>pima@gmail.com</td>
+                        <td>some address</td>
+                        <td>10</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    <tr>
+                        <td>Keith Pembroke</td>
+                        <td>10/11/1979</td>
+                        <td>07951938363</td>
+                        <td>keith.pembroke@gmail.com</td>
+                        <td>some address</td>
+                        <td>2</td>
+                        <td><a href="#"><i class="fas fa-info-circle"></i></a></td>
+                        <td><a href="#">make admin</i></a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer"></div>
+        </div>
+    </div>
+</div>
